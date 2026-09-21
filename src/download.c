2a459477af4f08995ccb933c61da87a338a41b38
oleksandr.ubuntu <kolexandr1983@gmail.com>
2026-09-22 02:23:04 +0300

    add 15 second loop to analyze speed precisely. cleared build folders and files

--- src/download.c
+++ src/download.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <time.h>
+
 #include "download.h"
 
 typedef struct {
@@ -19,7 +21,16 @@ static double calculate_mbps(TransferStats *stats, double seconds){
     return ((double)stats->bytes * 8) / (1000000.0 * seconds);
 }
 
+static double get_time_seconds(){
+    struct timespec ts;
+
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+
+    return (double)ts.tv_sec + (double)ts.tv_nsec / 1000000000.0;
+}
+
 double download_test(const char *URL){
+    const double time_for_download = 15.0; 
     CURL *curl = curl_easy_init();
 
     if (curl == NULL){
@@ -31,39 +42,74 @@ double download_test(const char *URL){
     curl_easy_setopt(curl, CURLOPT_URL, URL);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &stats);
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15L);
+    // curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15L);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // for developing
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/8.18.0");
 
-    result = curl_easy_perform(curl);
+    // for development
 
-    if (result != CURLE_OK && result != CURLE_OPERATION_TIMEDOUT){
-        printf("Download failed: %s\n", curl_easy_strerror(result));
-        curl_easy_cleanup(curl);
-        return -1;
+    // curl_easy_getinfo(
+    //     curl,
+    //     CURLINFO_RESPONSE_CODE,
+    //     &response_code
+    // );
+
+    // printf("HTTP status: %ld\n", response_code);
+    // printf("CURL result: %d - %s\n", result, curl_easy_strerror(result));
+    long response_code = 0;
+    double local_start, total_time, elapsed, remaining;
+    long remaining_ms;
+    int count = 1;
+    double start = get_time_seconds();
+
+    while (1){
+        elapsed = get_time_seconds() - start;
+
+        if (elapsed >= time_for_download){
+            break;
+        }
+
+        remaining = time_for_download - elapsed;
+        remaining_ms = (long)(remaining * 1000);
+
+        if (remaining_ms < 1) break;
+
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, remaining_ms);
+        
+        
+        local_start = get_time_seconds();
+        result = curl_easy_perform(curl);
+        
+        if (result == CURLE_OPERATION_TIMEDOUT) break;
+
+        if (result != CURLE_OK){
+            printf("Download failed: %s\n", curl_easy_strerror(result));
+            curl_easy_cleanup(curl);
+            return -1;
+        }
+
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+
+        if (response_code < 200 || response_code >= 300){
+            printf("Server returned HTTP %ld\n", response_code);
+            curl_easy_cleanup(curl);
+            return -1;
+        }
+      
+        total_time = get_time_seconds() - local_start;
+        printf("    Download No.%d\n", count);
+        printf("    Downloaded time: %.3f seconds\n", total_time);
+        // printf("    Downloaded: %lld bytes\n", (long long)stats.bytes);
+        count++;
     }
 
-    // for development
-    long response_code = 0;
+    total_time = get_time_seconds() - start;
+    double mbps = calculate_mbps(&stats, total_time); 
 
-    curl_easy_getinfo(
-        curl,
-        CURLINFO_RESPONSE_CODE,
-        &response_code
-    );
-
-    printf("HTTP status: %ld\n", response_code);
-    printf("CURL result: %d - %s\n", result, curl_easy_strerror(result));
-
-    double total_time = 0;
-
-    curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &total_time);
-    
-    double mbps = calculate_mbps(&stats, total_time);
-
-    printf("Downloaded: %lld bytes\n", (long long)stats.bytes);
+    printf("Download has finished. RESULTS:\n");
+    printf("Downloaded speed: %.2f Mbps/s\n", mbps);
     printf("Downloaded time: %.3f seconds\n", total_time);
-    printf("Downloaded speed: %.2f mbps/s\n", mbps);
+    printf("Downloaded: %lld bytes\n", (long long)stats.bytes);
 
     curl_easy_cleanup(curl);
 
